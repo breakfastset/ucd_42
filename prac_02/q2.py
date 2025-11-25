9e82be27ede14246711c228eeecfa822e01b78c0
breakfastset <34071068+breakfastset@users.noreply.github.com>
2025-11-25 20:58:45 +0800

    Add prac 02

---
+++ prac_02/q2.py
@@ -0,0 +1,12 @@
+a = 13
+b = 5
+
+print(f"Value a is {type(a)}")
+print(f"Value b is {type(b)}")
+
+print(f"{a} + {b} = {a + b}")
+print(f"{a} - {b} = {a - b}")
+print(f"{a} * {b} = {a * b}")
+print(f"{a} / {b} = {a / b}")
+print(f"{a} % {b} = {a % b}")
+print(f"{a} ** {b} = {a ** b}")
---
+++ prac_02/q3.py
@@ -0,0 +1,24 @@
+# q3a
+top = 7 - 4
+bottom = 5 - 3
+result = top / bottom
+print(result)
+
+# q3b
+top = 1
+bottom = 1 - 32 ** 2
+result = top / bottom
+print(result)
+
+# q3c
+term_1_top = 1 + 2.5
+term_1_bottom = 3 * 5
+term_1 = term_1_top / term_1_bottom
+term_2 = 2.9
+term_3 = 3 * 2
+
+result = (term_1 - term_2 + term_3) ** 2
+print(result)
+
+
+
